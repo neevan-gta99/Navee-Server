5fe7d5e12e0d94ffb30f9738ee67ca793b49dd1b
neevan-gta99 <naveengta8581@gmail.com>
2025-11-17 18:20:26 +0530

    Update addProductRoutes.js

--- routes/addProductRoutes.js
+++ routes/addProductRoutes.js
@@ -40,4 +40,40 @@ router.post('/men-bottomwear/bulk/xlsx', parser.busboyParserForXL,bulkProductsIm
 router.post('/men-footwear/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.menFootmwear);
 router.post('/men-footwear/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.menFootmwear);
 
+router.post('/women-ethnic/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.womenEthnic);
+router.post('/women-ethnic/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.womenEthnic);
+
+router.post('/women-western/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.womenWestern);
+router.post('/women-western/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.womenWestern);
+
+router.post('/women-footwear/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.womenFootwear);
+router.post('/women-footwear/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.womenFootwear);
+
+router.post('/boys-brands/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.boysBrands);
+router.post('/boys-brands/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.boysBrands);
+
+router.post('/girls-grands/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.girlsGrands);
+router.post('/girls-grands/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.girlsGrands);
+
+router.post('/mens-watches-and-accessories/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.menWA);
+router.post('/mens-watches-and-accessories/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.menWA);
+
+router.post('/wommens-watches-and-accessories/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.womenWA);
+router.post('/wommens-watches-and-accessories/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.womenWA);
+
+router.post('/boys-watches-and-accessories/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.boysWA);
+router.post('/boys-watches-and-accessories/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.boysWA);
+
+router.post('/girls-watches-and-accessories/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.girlsWA);
+router.post('/girls-watches-and-accessories/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.girlsWA);
+
+router.post('/bags/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.bags);
+router.post('/bags/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.bags);
+
+router.post('/suitcases/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.suitcases);
+router.post('/suitcases/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.suitcases);
+
+router.post('/luggages/bulk/csv', parser.busboyParserForCSV, parser.csvParser, product_Validator.sheetDataValidation, bulkProductsImagesUploader.allBulkUploader, addProduct.luggage);
+router.post('/luggages/bulk/xlsx', parser.busboyParserForXL,bulkProductsImagesUploader.allBulkUploader, addProduct.luggage);
+
 export default router;
